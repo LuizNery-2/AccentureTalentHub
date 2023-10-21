9d1e3f977d3bfacbc228457af6a289903bec6c06
Jhonnatas Mendonça <jhonnatas.m2000@gmail.com>
2023-10-21 05:15:21 -0300

    Classes model, controller, dto e repository criadas.

---
+++ src/main/java/com/accenture/accenturetalenthub/repositories/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.accenture.accenturetalenthub.repositories;
+
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.accenture.accenturetalenthub.models.UsuarioModel;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
+    Optional<UsuarioModel> findById(UUID IdUsuario);
+}
+
