3d2a8e0c76b1569709a7eb556f8251e659f83be5
Jhonnatas Mendonça <jhonnatas.m2000@gmail.com>
2023-11-11 11:56:09 -0300

    Importações nulas removidas

--- src/main/java/com/accenture/accenturetalenthub/repositories/InteresseRepository.java
+++ src/main/java/com/accenture/accenturetalenthub/repositories/InteresseRepository.java
@@ -4,8 +4,6 @@ import com.accenture.accenturetalenthub.models.InteresseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface InteresseRepository extends JpaRepository<InteresseModel, Long> {
 }
