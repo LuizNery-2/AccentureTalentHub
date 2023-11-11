a0da9fc6bb976dc77927b2185c946e357f7c183f
LuizNery-2 <luiz.nery@nave.org.br>
2023-11-11 16:36:41 -0300

    Finalizando o serviço de associação

--- src/main/java/com/accenture/accenturetalenthub/services/AssociatesEntitiesService.java
+++ src/main/java/com/accenture/accenturetalenthub/services/AssociatesEntitiesService.java
@@ -1,13 +1,7 @@
 package com.accenture.accenturetalenthub.services;
 
-import com.accenture.accenturetalenthub.models.CursoModel;
-import com.accenture.accenturetalenthub.models.InteresseModel;
-import com.accenture.accenturetalenthub.models.SalaModel;
-import com.accenture.accenturetalenthub.models.UsuarioModel;
-import com.accenture.accenturetalenthub.repositories.CursoRepository;
-import com.accenture.accenturetalenthub.repositories.InteresseRepository;
-import com.accenture.accenturetalenthub.repositories.SalaRepository;
-import com.accenture.accenturetalenthub.repositories.UsuarioRepository;
+import com.accenture.accenturetalenthub.models.*;
+import com.accenture.accenturetalenthub.repositories.*;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +17,10 @@ public class AssociatesEntitiesService {
     UsuarioRepository usuarioRepository;
     @Autowired
     SalaRepository salaRepository;
+    @Autowired
+    ModuloRepository moduloRepository;
+    @Autowired
+    AulasRepository aulasRepository;
 
 
 
@@ -31,6 +29,13 @@ public class AssociatesEntitiesService {
 
         curso.getInteresses().add(interesse);
         interesse.getCursos().add(curso);
+        cursoRepository.save(curso);
+        interesseRepository.save(interesse);
+    }
+    @Transactional
+    public void removerAssociacaoCursoInteresse(CursoModel curso, InteresseModel interesse){
+        curso.getInteresses().remove(interesse);
+        interesse.getCursos().remove(curso);
         cursoRepository.save(curso);
         interesseRepository.save(interesse);
     }
@@ -43,6 +48,31 @@ public class AssociatesEntitiesService {
         cursoRepository.save(curso);
         salaRepository.save(sala);
     }
+    @Transactional
+    public void removerAssociacaoCursoSalas(CursoModel curso, SalaModel sala)
+    {
+        curso.getSalas().remove(curso);
+        sala.getCursos().remove(sala);
+        cursoRepository.save(curso);
+        salaRepository.save(sala);
+    }
+
+    @Transactional
+    public void associarModulosAulas(ModuloModel modulo, AulaModel aula)
+    {
+        modulo.getAulas().add(aula);
+        aula.getModulos().add(modulo);
+        moduloRepository.save(modulo);
+        aulasRepository.save(aula);
+    }
+    @Transactional
+    public void removerAssociacaoModulosAulas(ModuloModel modulo, AulaModel aula)
+    {
+        modulo.getAulas().remove(aula);
+        aula.getModulos().remove(modulo);
+        moduloRepository.save(modulo);
+        aulasRepository.save(aula);
+    }
 
     @Transactional
     public void associarUsuarioCurso(UsuarioModel usuario, CursoModel curso)
@@ -52,6 +82,15 @@ public class AssociatesEntitiesService {
         usuarioRepository.save(usuario);
         cursoRepository.save(curso);
     }
+    @Transactional
+    public void removerAssociacaoUsuarioCurso(UsuarioModel usuario, CursoModel curso)
+    {
+        usuario.getCursos().remove(curso);
+        curso.getUsuarios().remove(usuario);
+        usuarioRepository.save(usuario);
+        cursoRepository.save(curso);
+
+    }
 
     @Transactional
     public void associarUsuarioInteresses(UsuarioModel usuario, InteresseModel interesse)
@@ -60,7 +99,34 @@ public class AssociatesEntitiesService {
             interesse.getUsuarios().add(usuario);
 
             usuarioRepository.save(usuario);
+            interesseRepository.save(interesse);
 
+    }
+    @Transactional
+    public void removerAssociacaoUsuarioInteresses(UsuarioModel usuario, InteresseModel interesse)
+    {
+        usuario.getInteresses().remove(interesse);
+        interesse.getUsuarios().remove(usuario);
+
+        usuarioRepository.save(usuario);
+        interesseRepository.save(interesse);
+
+    }
+    @Transactional
+    public void  associarUsuariosSalas(UsuarioModel usuario, SalaModel sala)
+    {
+        usuario.getSalas().add(sala);
+        sala.getUsuarios().add(usuario);
+        usuarioRepository.save(usuario);
+        salaRepository.save(sala);
+    }
+    @Transactional
+    public void  removerAssociacaoUsuariosSalas(UsuarioModel usuario, SalaModel sala)
+    {
+        usuario.getSalas().remove(sala);
+        sala.getUsuarios().remove(usuario);
+        usuarioRepository.save(usuario);
+        salaRepository.save(sala);
     }
 
 
