f6539559fe5e84bc45a82768a45250d16206612e
Jhonnatas Mendonça <jhonnatas.m2000@gmail.com>
2023-11-15 17:02:17 -0300

    Função: atualizar interesses pelo navegador desenvolvida.

--- src/main/java/com/accenture/accenturetalenthub/controllers/AssociationsController.java
+++ src/main/java/com/accenture/accenturetalenthub/controllers/AssociationsController.java
@@ -15,9 +15,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 public class AssociationsController {
@@ -60,30 +63,25 @@ public class AssociationsController {
         return ResponseEntity.status(HttpStatus.OK).body("Interesses adicionados ao Curso com sucesso");
     }
 
-    // @PostMapping("usuariosInteresses/{idUsuario}")
-    // public ResponseEntity<String> saveUsuarioInteresse(@PathVariable(value = "idUsuario") UUID idUsuario, @RequestBody List<Long> idsInteresses)
-    // {
-    //     Optional<UsuarioModel> usuarioO = usuarioRepository.findById(idUsuario);
-    //     if (usuarioO.isEmpty())
-    //     {
-    //         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
-    //     }
-    //     var usuarioModel = usuarioO.get();
-    //     for (long idInteresse : idsInteresses)
-    //     {
-    //         Optional<InteresseModel> interesseO = interesseRepository.findById(idInteresse);
-    //         if (interesseO.isEmpty()) {
-    //             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Interesse com ID " + idInteresse + " não encontrado");
-    //         }
-    //         var interesseModel = interesseO.get();
-    //         associatesEntitiesService.associarUsuarioInteresses(usuarioModel, interesseModel);
-    //     }
-    //     return ResponseEntity.status(HttpStatus.OK).body("Interesses adcionados ao usuário com sucesso");
-    // }
+
+
     @PostMapping("usuariosInteresses/{idUsuario}")
     public ResponseEntity<String> saveUsuarioInteresse(
-        @PathVariable(value = "idUsuario") UUID idUsuario,
-        @RequestBody List<String> nomesInteresses) {
+            @PathVariable(value = "idUsuario") UUID idUsuario,
+            @RequestBody Map<String, String> requestBody) {
+
+        // Certifique-se de que a chave "idsInteresses" está presente no corpo da solicitação
+        if (!requestBody.containsKey("idsInteresses")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A chave 'idsInteresses' é obrigatória no corpo da solicitação");
+        }
+
+        // Obtenha a string de IDs de interesse a partir do corpo da solicitação
+        String idsInteressesString = requestBody.get("idsInteresses");
+
+        // Converta a string em uma lista de IDs de interesse
+        List<Long> idsInteresses = Arrays.stream(idsInteressesString.split(","))
+                .map(Long::valueOf)
+                .collect(Collectors.toList());
 
         Optional<UsuarioModel> usuarioO = usuarioRepository.findById(idUsuario);
         if (usuarioO.isEmpty()) {
@@ -91,11 +89,11 @@ public class AssociationsController {
         }
 
         var usuarioModel = usuarioO.get();
-        
-        for (String nomeInteresse : nomesInteresses) {
-            Optional<InteresseModel> interesseO = interesseRepository.findByNome(nomeInteresse);
+
+        for (long idInteresse : idsInteresses) {
+            Optional<InteresseModel> interesseO = interesseRepository.findById(idInteresse);
             if (interesseO.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Interesse com nome '" + nomeInteresse + "' não encontrado");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Interesse com ID " + idInteresse + " não encontrado");
             }
 
             var interesseModel = interesseO.get();
